8dfd69ef5a1742244de7274377baf169d4ca431e
BasmaAbdelhakim <basmaabdelhakim95@gmail.com>
2020-06-03 22:36:40 +0200

    Editing the ELF_Parser python script to accept the input file as an argument. Signed-off-by: BasmaAbdelhakim <basmaabdelhakim95@gmail.com>

--- Python-script/ELF_Parser.py
+++ Python-script/ELF_Parser.py
@@ -3,69 +3,73 @@ from elftools.common.py3compat import bytes2str
 from elftools.elf.elffile import ELFFile
 
 def main():
-    with open('bootloader-dummy-app.elf', 'rb') as f:
-        e = ELFFile(f)
-        TextPhysicalAddress = 0
-        APPSize = 0
-        EntryPoint = e['e_entry']
-        DataPhysicalAddress = 0
-        DataSize = 0
-        
-        print('Entry point = ' + hex(e['e_entry']))
+    if(len(sys.argv)==1):
+        print("Please add ELf file as argument to the file")
+    else:
+        FileName=sys.argv[1]
+        with open(FileName, 'rb') as f:
+            e = ELFFile(f)
+            TextPhysicalAddress = 0
+            APPSize = 0
+            EntryPoint = e['e_entry']
+            DataPhysicalAddress = 0
+            DataSize = 0
+            
+            print('Entry point = ' + hex(e['e_entry']))
 
-        for i in range(int(e.num_segments())):
-            segment = e.get_segment(i)
-            physicalAddress = hex(segment.header['p_paddr'])
-            offest = hex(segment.header['p_offset'])
-            size = hex(segment.header['p_filesz'])
-            APPSize += segment.header['p_filesz']
+            for i in range(int(e.num_segments())):
+                segment = e.get_segment(i)
+                physicalAddress = hex(segment.header['p_paddr'])
+                offest = hex(segment.header['p_offset'])
+                size = hex(segment.header['p_filesz'])
+                APPSize += segment.header['p_filesz']
 
-            name = ''
-            if i == 0:
-                name = '.text'
-                TextPhysicalAddress = segment.header['p_paddr']
-            elif i == 1:
-                name = '.data'
-                DataPhysicalAddress = segment.header['p_paddr']
-                DataSize = segment.header['p_filesz']
-            elif i == 2:
-                name = '.bss'
+                name = ''
+                if i == 0:
+                    name = '.text'
+                    TextPhysicalAddress = segment.header['p_paddr']
+                elif i == 1:
+                    name = '.data'
+                    DataPhysicalAddress = segment.header['p_paddr']
+                    DataSize = segment.header['p_filesz']
+                elif i == 2:
+                    name = '.bss'
 
-            print('Section name = ' + name)
-            print('Section physical address = ' + physicalAddress)
-            print('Section offest = ' + offest)
-            print('Section size = ' + size+'\n')
-              
-        # the text section
-        #textSec = e.get_section_by_name('.text')
-        textSec = e.get_segment(0)
-        textval = textSec.data()
-        
-        #data section
-        datasec = e.get_segment(1)
-        dataval = datasec.data()
+                print('Section name = ' + name)
+                print('Section physical address = ' + physicalAddress)
+                print('Section offest = ' + offest)
+                print('Section size = ' + size+'\n')
+                  
+            # the text section
+            #textSec = e.get_section_by_name('.text')
+            textSec = e.get_segment(0)
+            textval = textSec.data()
+            
+            #data section
+            datasec = e.get_segment(1)
+            dataval = datasec.data()
 
-        file = open('TEXT_FILE.txt', 'wb')
-        file.write(textval)
-        file.close
+            file = open('TEXT_FILE.txt', 'wb')
+            file.write(textval)
+            file.close
 
-        file = open('DATA_FILE.txt', 'wb')
-        file.write(dataval)
-        file.close
+            file = open('DATA_FILE.txt', 'wb')
+            file.write(dataval)
+            file.close
 
-        print("EntryPoint = " + hex(EntryPoint))
-        print("APPSize = " + hex(APPSize))
-        print("TextPhysicalAddress = " + hex(TextPhysicalAddress))
-        print("DataPhysicalAddress = " + hex(DataPhysicalAddress))
-        print("DataSize = " + hex(DataSize))
-        
-        file = open('INFO_FILE.txt', 'wb')
-        file.write(int_to_bytes(TextPhysicalAddress))
-        file.write(int_to_bytes(APPSize))
-        file.write(int_to_bytes(EntryPoint))
-        file.write(int_to_bytes(DataPhysicalAddress))
-        file.write(int_to_bytes(DataSize))
-        file.close
+            print("EntryPoint = " + hex(EntryPoint))
+            print("APPSize = " + hex(APPSize))
+            print("TextPhysicalAddress = " + hex(TextPhysicalAddress))
+            print("DataPhysicalAddress = " + hex(DataPhysicalAddress))
+            print("DataSize = " + hex(DataSize))
+            
+            file = open('INFO_FILE.txt', 'wb')
+            file.write(int_to_bytes(TextPhysicalAddress))
+            file.write(int_to_bytes(APPSize))
+            file.write(int_to_bytes(EntryPoint))
+            file.write(int_to_bytes(DataPhysicalAddress))
+            file.write(int_to_bytes(DataSize))
+            file.close
 
 def int_to_bytes(x: int) -> bytes:
     return x.to_bytes(4, byteorder="little", signed=False)
